774f0f11478311a71cbaa0877171b31c2eb00855
Crysanthe <22611108+anacronox@users.noreply.github.com>
2022-06-14 22:09:31 +0200

    First commit

---
+++ wp-smpt-mail.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action( 'phpmailer_init', 'customPhpmailerSmtp' );
+function customPhpmailerSmtp( $phpmailer ) {
+    $phpmailer->isSMTP();     
+    $phpmailer->Host = SMTP_server;  
+    $phpmailer->SMTPAuth = SMTP_AUTH;
+    $phpmailer->Port = SMTP_PORT;
+    $phpmailer->Username = SMTP_username;
+    $phpmailer->Password = SMTP_password;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->From = SMTP_FROM;
+    $phpmailer->FromName = SMTP_NAME;
+}
