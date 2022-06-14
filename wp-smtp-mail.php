f91c5003fcb0e2ac92a14171e4bb1e54a7219588
Crysanthe <22611108+anacronox@users.noreply.github.com>
2022-06-14 22:25:32 +0200

    add plugin comment

--- wp-smtp-mail.php
+++ wp-smtp-mail.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Plugin Name: Module SMTP MAIL
+ * Plugin URI: https://github.com/anacronox/wp-smtp-mail
+ * Description: Simple smtp configuration for wordpress
+ * Version: 1.0
+ * Author: Anacronox
+ * Author URI: https://github.com/anacronox/
+ */
+
+
 
 add_action( 'phpmailer_init', 'customPhpmailerSmtp' );
 function customPhpmailerSmtp( $phpmailer ) {
